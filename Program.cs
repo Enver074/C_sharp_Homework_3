1599306cc981de0ce0b69b4e7545c50dd55e49cf
Enver Ismailov <113299648+Enver074@users.noreply.github.com>
2022-10-08 21:34:08 +0300

    Второе задание

--- Program.cs
+++ Program.cs
@@ -33,20 +33,49 @@
 // }
 
 
-Console.WriteLine("Введите пятизначное число");
-string number = Console.ReadLine()!;
-if (number.Length == 5)
-    {
-    if ((number[0])==(number[4])&&(number[1])==(number[3]))
-        {
-        Console.WriteLine("Да");
-        }
-    else
-        {
-        Console.WriteLine("Нет");
-        }
-    }
-else
-    {  
-    Console.WriteLine("Число не пятизначное");
-    }
+// Console.WriteLine("Введите пятизначное число");
+// string number = Console.ReadLine()!;
+// if (number.Length == 5)
+//     {
+//     if ((number[0])==(number[4])&&(number[1])==(number[3]))
+//         {
+//         Console.WriteLine("Да");
+//         }
+//     else
+//         {
+//         Console.WriteLine("Нет");
+//         }
+//     }
+// else
+//     {  
+//     Console.WriteLine("Число не пятизначное");
+//     }
+
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+Console.Write("Введите координаты x1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координаты y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координаты z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координаты x2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координаты y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координаты z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double len = Math.Sqrt((Math.Pow(x1-x2, 2))+(Math.Pow(y1-y2, 2))+(Math.Pow(z1-z2, 2)));
+Console.WriteLine($"Расстояние между точками {len:f2}");
