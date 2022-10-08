c6265cf8a4eb22c00c2571621675c4a6ca4e733b
Enver Ismailov <113299648+Enver074@users.noreply.github.com>
2022-10-08 20:45:16 +0300

    Первое задание 'через строку'

--- Program.cs
+++ Program.cs
@@ -11,23 +11,42 @@
 // Задача 21
 
 
+// Console.WriteLine("Введите пятизначное число");
+// int number = int.Parse(Console.ReadLine()!);
+// if ((number>100000)||(number<10000))
+// {
+//     Console.WriteLine("Введеное число не пятизначное");
+// }
+// if ((number>=10000) && (number<100000))
+// {
+//     int a = number / 10000;
+//     int b = (number / 1000) % 10;
+//     int c = (number / 100) % 10;
+//     int d = (number / 10) % 10;
+//     int e = number % 10;
+
+//     if ((a == e)&&(b == d))
+//         Console.WriteLine("ДА");
+//     else
+//         Console.WriteLine("Нет");
+
+// }
+
+
 Console.WriteLine("Введите пятизначное число");
-int number = int.Parse(Console.ReadLine()!);
-if ((number>100000)||(number<10000))
-{
-    Console.WriteLine("Введеное число не пятизначное");
-}
-if ((number>=10000) && (number<100000))
-{
-    int a = number / 10000;
-    int b = (number / 1000) % 10;
-    int c = (number / 100) % 10;
-    int d = (number / 10) % 10;
-    int e = number % 10;
-
-    if ((a == e)&&(b == d))
-        Console.WriteLine("ДА");
+string number = Console.ReadLine()!;
+if (number.Length == 5)
+    {
+    if ((number[0])==(number[4])&&(number[1])==(number[3]))
+        {
+        Console.WriteLine("Да");
+        }
     else
+        {
         Console.WriteLine("Нет");
-
-}
+        }
+    }
+else
+    {  
+    Console.WriteLine("Число не пятизначное");
+    }
